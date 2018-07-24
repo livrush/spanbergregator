1d3abe335daeceecfcd7e4391788047688956660
Olivia Sabo-Rush <liv@Olivia-SaboRushs-Computer.local>
2018-07-24 19:36:42 -0400

    update vote view

--- client/src/components/VoteInfo.js
+++ client/src/components/VoteInfo.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import Vote from './Vote.js';
+import Vote from './Vote';
 import uuid from 'uuid/v1';
 
 const VoteInfo = ({ votes }) => {
     const voteDivs = votes.map(voteInfo =><Vote key={uuid()} vote={voteInfo}></Vote>);
 
     return (
-    <div className="vote-info list-group-item">
-        <a href="https://www.govtrack.us/congress/members/david_brat/412605" target="_blank">
+    <ul className="list-group">
+        {/* <a href="https://www.govtrack.us/congress/members/david_brat/412605" target="_blank">
             <button className="btn btn-danger">GovTrack</button>
-        </a>
-        <ul>
+        </a> */}
             {voteDivs}
-        </ul>
-    </div>
+    </ul>
 )
 };
 

0ef93fdb641af264da86594c67d9ccd1c130c7c9
Olivia Sabo-Rush <liv@Olivia-SaboRushs-Computer.local>
2018-07-24 12:18:36 -0400

    add articles component

---
+++ src/components/Articles.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Articles = ({ article }) => (
+    <li className="article list-group-item">
+        <div className="row">
+            <div className="col-4">
+                <img className="article-img" src={article.urlToImage} />
+                <p> { article.source.name } </p>
+                <p> { article.publishedAt } </p>
+            </div>
+            <div className="col-8">
+                <p>
+                    <a href={ article.url } target="_blank">
+                        { article.title }
+                    </a>
+                </p>
+                <p>
+                    { article.description }
+                </p>
+            </div>
+        </div>
+    </li>
+);
+
+export default Articles;

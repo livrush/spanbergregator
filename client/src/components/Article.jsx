b329a920b0febf3c7d41ba2fb813fd205d74659f
Liv <w.phrush@gmail.com>
2018-07-26 19:13:13 -0400

    update article to cut off text

--- client/src/components/Article.jsx
+++ client/src/components/Article.jsx
@@ -1,27 +1,28 @@
-import React from 'react';
-import moment from 'moment';
-
+import React from "react";
+import moment from "moment";
 
 const Article = ({ article }) => (
-    <li className="article list-group-item">
-        <div className="row">
-            <div className="col-4">
-                <img className="article-img" alt="Article Img" src={article.urlToImage} />
-                <p> { article.source.name } </p>
-                <p> { moment(article.publishedAt).format("dddd, MMMM Do") } </p>
-            </div>
-            <div className="col-8">
-                <p>
-                    <a href={ article.url } target="_blank">
-                        { article.title }
-                    </a>
-                </p>
-                <p>
-                    { article.description }
-                </p>
-            </div>
-        </div>
-    </li>
+  <li className="article list-group-item">
+    <div className="row">
+      <div className="col-4">
+        <img
+          className="article-img"
+          alt="Article Img"
+          src={article.urlToImage}
+        />
+        <p className="text-ellipsis"> {article.source.name} </p>
+        <p> {moment(article.publishedAt).format("dddd, MMMM Do")} </p>
+      </div>
+      <div className="col-8">
+        <p>
+          <a href={article.url} target="_blank">
+            {article.title}
+          </a>
+        </p>
+        <p>{article.description}</p>
+      </div>
+    </div>
+  </li>
 );
 
 export default Article;

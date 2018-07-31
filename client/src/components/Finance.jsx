4f5583fd7f76362a9b1572fa5fab735617163748
Liv <w.phrush@gmail.com>
2018-07-31 18:14:17 -0400

    add finance component

---
+++ client/src/components/Finance.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import moment from "moment";
+
+const Finance = ({ data }) => {
+  console.log(data);
+  return (
+  <div className="card">
+    <div className="row">
+      <div className="col-6">
+      <h1>Placeholder</h1>
+      </div>
+      <div className="col-6">
+      </div>
+    </div>
+  </div>
+  )
+};
+
+export default Finance;

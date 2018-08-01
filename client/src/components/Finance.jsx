c64cf4d68f260650b040e491f749b1f0544be142
Liv <w.phrush@gmail.com>
2018-08-01 12:55:48 -0400

    update finances component

--- client/src/components/Finance.jsx
+++ client/src/components/Finance.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import moment from "moment";
-import bb from "billboard.js";
+import React from 'react';
+import moment from 'moment';
+import bb from 'billboard.js';
 
 const Finance = ({ data }) => {
   console.log(data);
@@ -8,58 +8,102 @@ const Finance = ({ data }) => {
     var bar = bb.generate({
       bindto: `#${data.id}-breakdown`,
       data: {
-        type: "bar",
+        type: 'bar',
         columns: [
-          ["Total Contributions", data.total_contributions],
-          ["Total Disbursements", data.total_disbursements],
-          ["Independent Expenditures", data.independent_expenditures]
+          ['Total Contributions', data.total_contributions],
+          ['Total Disbursements', data.total_disbursements],
+          ['Independent Expenditures', data.independent_expenditures]
         ]
+      }
+    });
+
+    var usage = bb.generate({
+      bindto: `#${data.id}-usage`,
+      data: {
+        type: 'donut',
+        columns: [
+          ['Available', data.total_contributions - data.total_disbursements],
+          ['Disbursed', data.total_disbursements]
+        ],
+        colors: {
+          Available: '#28a745',
+          Disbursed: '#dc3545'
+        }
       }
     });
 
     var pie = bb.generate({
       bindto: `#${data.id}-total-from`,
       data: {
-        type: "donut",
+        type: 'donut',
         columns: [
-          ["From Individuals", data.total_from_individuals],
-          ["From PACs", data.total_from_pacs]
-        ]
+          ['Individuals', data.total_from_individuals],
+          ['PACs', data.total_from_pacs]
+        ],
+        colors: {
+          Individuals: '#1b4062',
+          PACs: '#8DC6E8'
+        }
+      },
+      interaction: {
+        enabled: false
       }
     });
 
     console.log(bar);
   });
 
-  return <div className="">
-      <h1>{data.name}</h1>
+  return (
+    <div className="container">
+      <a href="">{data.name}</a>
       <div class="row">
-        <div class="col-2 text-left">Begin:</div>
-        <div class="col-4 text-right">${data.begin_cash}</div>
-        <div class="col-4 text-left">
-          on {moment(data.date_coverage_from).format("MM/DD/YY")}
+        <div class="col-6 text-left font-weight-bold">
+          Begin {moment(data.date_coverage_from).format('MM/DD/YY')}:
         </div>
+        <div class="col-6 text-right">${data.begin_cash}</div>
       </div>
       <div class="row">
-        <div class="col-2 text-left">End:</div>
-        <div class="col-4 text-right">${data.end_cash}</div>
-        <div class="col-4 text-left">
-          on {moment(data.date_coverage_to).format("MM/DD/YY")}
+        <div class="col-6 text-left font-weight-bold">
+          End {moment(data.date_coverage_to).format('MM/DD/YY')}:
         </div>
+        <div class="col-6 text-right">${data.end_cash}</div>
       </div>
       <h2>Contributions</h2>
       {/* <div id={data.id + "-breakdown"} /> */}
-      <p>
-        <strong>Total Contributions:</strong> ${data.total_contributions}
-      </p>
-      <p>
-        <strong>Total Disbursements:</strong> ${data.total_disbursements},
-      </p>
-      <p>
-        <strong>Independent Expenditures</strong> ${data.independent_expenditures}
-      </p>
-      <div id={data.id + "-total-from"} />
-    </div>;
+      <div id={data.id + '-usage'} />
+
+      <div className="row">
+        <div className="col-6 font-weight-bold text-left">
+          Total
+          <span className="text-success font-weight-bold"> Contributions:</span>
+        </div>
+        <div className="col-6 text-right">${data.total_contributions}</div>
+      </div>
+      <div className="row">
+        <div className="col-6 font-weight-bold text-left">
+          Total
+          <span className="text-danger font-weight-bold"> Disbursements:</span>
+        </div>
+        <div className="col-6 text-right">${data.total_disbursements}</div>
+      </div>
+      <div className="row">
+        <div className="col-6 font-weight-bold text-left">
+          Total
+          <span className="text-success font-weight-bold"> Available:</span>
+        </div>
+        <div className="col-6 text-right">
+          ${(data.total_contributions - data.total_disbursements).toFixed(2)}
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-6 font-weight-bold text-left">
+          Independent Expenditures:
+        </div>
+        <div className="col-6 text-right">${data.independent_expenditures}</div>
+      </div>
+      <div id={data.id + '-total-from'} />
+    </div>
+  );
 };
 
 export default Finance;

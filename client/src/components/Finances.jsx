d22647828862e3da8e538f9b62219521230ab410
Liv <w.phrush@gmail.com>
2018-08-01 12:55:38 -0400

    make mobile friendly

--- client/src/components/Finances.jsx
+++ client/src/components/Finances.jsx
@@ -3,15 +3,14 @@ import moment from "moment";
 import Finance from './Finance';
 
 const Finances = ({ data }) => {
-  console.log(data);
   const [candidate, opponent] = data;
   return (
-  <div className="card">
+  <div className="card py-4">
     <div className="row">
-      <div className="col-6">
+      <div className="col-12 col-sm-6">
         <Finance data={candidate}></Finance>
       </div>
-      <div className="col-6">
+      <div className="col-12 col-sm-6">
         <Finance data={opponent}></Finance>
       </div>
     </div>

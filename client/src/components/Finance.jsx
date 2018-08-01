a43653ea7b16d6936a75c12d64eefff9586ccd75
Liv <w.phrush@gmail.com>
2018-08-01 10:09:43 -0400

    swap bar graph for simple table

--- client/src/components/Finance.jsx
+++ client/src/components/Finance.jsx
@@ -5,11 +5,10 @@ import bb from "billboard.js";
 const Finance = ({ data }) => {
   console.log(data);
   setTimeout(function() {
-
     var bar = bb.generate({
       bindto: `#${data.id}-breakdown`,
       data: {
-        type: "gauge",
+        type: "bar",
         columns: [
           ["Total Contributions", data.total_contributions],
           ["Total Disbursements", data.total_disbursements],
@@ -24,31 +23,43 @@ const Finance = ({ data }) => {
         type: "donut",
         columns: [
           ["From Individuals", data.total_from_individuals],
-          ["From PACs", data.total_from_pacs],
-        ],
-      },
+          ["From PACs", data.total_from_pacs]
+        ]
+      }
     });
 
-  console.log(bar);
+    console.log(bar);
   });
 
-  return (
-  <div className="">
-    <h1>{data.name}</h1>
-    <h2>Contributions</h2>
-    <div id={data.id + '-breakdown'}></div>
-    <p>
-      <strong>Total Contributions:</strong> {data.total_contributions}
-    </p>
-    <p>
-      <strong>Total Disbursements:</strong> {data.total_disbursements},
-    </p>
-    <p>
-      <strong>Independent Expenditures</strong> {data.independent_expenditures}
-    </p>
-    <div id={data.id + '-total-from'}></div>
-  </div>
-  );
+  return <div className="">
+      <h1>{data.name}</h1>
+      <div class="row">
+        <div class="col-2 text-left">Begin:</div>
+        <div class="col-4 text-right">${data.begin_cash}</div>
+        <div class="col-4 text-left">
+          on {moment(data.date_coverage_from).format("MM/DD/YY")}
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-2 text-left">End:</div>
+        <div class="col-4 text-right">${data.end_cash}</div>
+        <div class="col-4 text-left">
+          on {moment(data.date_coverage_to).format("MM/DD/YY")}
+        </div>
+      </div>
+      <h2>Contributions</h2>
+      {/* <div id={data.id + "-breakdown"} /> */}
+      <p>
+        <strong>Total Contributions:</strong> ${data.total_contributions}
+      </p>
+      <p>
+        <strong>Total Disbursements:</strong> ${data.total_disbursements},
+      </p>
+      <p>
+        <strong>Independent Expenditures</strong> ${data.independent_expenditures}
+      </p>
+      <div id={data.id + "-total-from"} />
+    </div>;
 };
 
 export default Finance;

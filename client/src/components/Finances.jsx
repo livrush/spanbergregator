c2cb4d44027eb3f43310cd397525b4e6d1ceb306
Liv <w.phrush@gmail.com>
2018-08-02 19:10:56 -0400

    update prop names

--- client/src/components/Finances.jsx
+++ client/src/components/Finances.jsx
@@ -3,15 +3,20 @@ import moment from "moment";
 import Finance from './Finance';
 
 const Finances = ({ data }) => {
-  const [candidate, opponent] = data;
+  const [
+    candidate,
+    opponent,
+    demCommittee,
+    repCommittee,
+  ] = data;
   return (
   <div className="card py-4">
     <div className="row">
       <div className="col-12 col-sm-6">
-        <Finance data={candidate}></Finance>
+        <Finance candidateInfo={candidate} committeeInfo={null}></Finance>
       </div>
       <div className="col-12 col-sm-6">
-        <Finance data={opponent}></Finance>
+        <Finance candidateInfo={opponent} committeeInfo={null}></Finance>
       </div>
     </div>
   </div>

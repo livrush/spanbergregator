b7d1c80a85f5b1f94b4025ffc5b9aec432bad753
Liv <w.phrush@gmail.com>
2018-08-02 18:54:21 -0400

    add committee FEC Id

--- client/src/config.js
+++ client/src/config.js
@@ -3,8 +3,10 @@ export default {
   candidateTwitter: 'SpanbergerVA07',
   candidateMemberId: null,
   candidateFECId: 'H8VA07094',
+  candidateCommitteeFECId: 'C00649913',
   opponentName: 'Dave Brat',
   opponentTwitter: 'RepDaveBrat',
   opponentMemberId: 'B001290',
   opponentFECId: 'H4VA07143',
+  opponentCommitteeFECId: 'C00554949'
 };

6655f78f8b835c0832d7cd0f231018e9f42a8f78
Liv <w.phrush@gmail.com>
2018-08-02 19:35:32 -0400

    begin adding committee info, styling

--- client/src/components/Finance.jsx
+++ client/src/components/Finance.jsx
@@ -3,7 +3,8 @@ import moment from 'moment';
 import bb from 'billboard.js';
 
 const Finance = ({ candidateInfo, committeeInfo }) => {
-  console.log(candidateInfo, committeeInfo);
+  console.log(candidateInfo);
+  console.log(committeeInfo);
   setTimeout(function() {
     var bar = bb.generate({
       bindto: `#${candidateInfo.id}-breakdown`,
@@ -52,40 +53,75 @@ const Finance = ({ candidateInfo, committeeInfo }) => {
         enabled: false,
       },
     });
-
-    console.log(bar);
   });
 
-  return (
-    <div className="container">
-      <a href="">{candidateInfo.name}</a>
-      <div className="w-100 border-bottom my-2"></div>
+  return <div className="container">
+      <a href="">{committeeInfo.name}</a>
+      <div className="w-100 border-bottom my-2" />
       <div className="row">
         <div className="col-12 col-md-6 text-left font-weight-bold">
-          Begin {moment(candidateInfo.date_coverage_from).format('MM/DD/YY')}:
+          Start {moment(committeeInfo.date_coverage_from).format('MM/DD/YY')}:
         </div>
-        <div className="col-12 col-md-6 text-right">${candidateInfo.begin_cash}</div>
+        <div className="col-12 col-md-6 text-right">
+          ${sanitizeNumber(committeeInfo.begin_cash)}
+        </div>
       </div>
       <div className="row">
         <div className="col-12 col-md-6 text-left font-weight-bold">
-          End {moment(candidateInfo.date_coverage_to).format('MM/DD/YY')}:
+          End {moment(committeeInfo.date_coverage_to).format('MM/DD/YY')}:
         </div>
-        <div className="col-12 col-md-6 text-right">${candidateInfo.end_cash}</div>
+        <div className="col-12 col-md-6 text-right">
+          ${sanitizeNumber(committeeInfo.end_cash)}
+        </div>
       </div>
-      <div className="w-100 border-bottom my-2"></div>
+      <div className="w-100 border-bottom my-2" />
       <div className="row">
         <div className="col-12 col-md-6 text-left font-weight-bold">
           Total
-          <span className="text-success font-weight-bold"> Contributions:</span>
+          <span className="text-success font-weight-bold">
+            {' '}
+            Contributions:
+          </span>
         </div>
-        <div className="col-12 col-md-6 text-right">${candidateInfo.total_contributions}</div>
+        <div className="col-12 col-md-6 text-right">
+         <span className="text-success">${sanitizeNumber(committeeInfo.total_contributions)}</span>
+        </div>
       </div>
       <div className="row">
         <div className="col-12 col-md-6 text-left font-weight-bold">
           Total
-          <span className="text-danger font-weight-bold"> Disbursements:</span>
+          <span className="text-info font-weight-bold">
+            {' '}
+            From Individuals:
+          </span>
         </div>
-        <div className="col-12 col-md-6 text-right">${candidateInfo.total_disbursements}</div>
+        <div className="col-12 col-md-6 text-right">
+         <span className="text-info">${sanitizeNumber(committeeInfo.total_from_individuals)}</span>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 col-md-6 text-left font-weight-bold">
+          Total
+          <span className="text-warning font-weight-bold">
+            {' '}
+            From PACs:
+          </span>
+        </div>
+        <div className="col-12 col-md-6 text-right">
+         <span className="text-warning">${sanitizeNumber(committeeInfo.total_from_pacs)}</span>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 col-md-6 text-left font-weight-bold">
+          Total
+          <span className="text-danger font-weight-bold">
+            {' '}
+            Disbursements:
+          </span>
+        </div>
+        <div className="col-12 col-md-6 text-right">
+         <span className="text-danger">${sanitizeNumber(committeeInfo.total_disbursements)}</span>
+        </div>
       </div>
       <div className="row">
         <div className="col-12 col-md-6 text-left font-weight-bold">
@@ -93,24 +129,110 @@ const Finance = ({ candidateInfo, committeeInfo }) => {
           <span className="text-success font-weight-bold"> Available:</span>
         </div>
         <div className="col-12 col-md-6 text-right">
-          ${(candidateInfo.total_contributions - candidateInfo.total_disbursements).toFixed(2)}
+         <span className="text-success">${sanitizeNumber(
+            committeeInfo.total_contributions -
+            committeeInfo.total_disbursements
+          )}</span>
         </div>
       </div>
       <div className="row">
         <div className="col-12 col-md-6 text-left font-weight-bold">
           Independent Expenditures:
         </div>
-        <div className="col-12 col-md-6 text-right">${candidateInfo.independent_expenditures}</div>
+        <div className="col-12 col-md-6 text-right">
+         <span className="text-default">${sanitizeNumber(committeeInfo.total_independent_expenditures)}</span>
+        </div>
       </div>
-      <div className="w-100 border-bottom my-2"></div>
+      <div className="w-100 border-bottom my-2" />
+
+
+
+
+
+
+
+
+      <a href="">{candidateInfo.name}</a>
+      <div className="w-100 border-bottom my-2" />
+      <div className="row">
+        <div className="col-12 col-md-6 text-left font-weight-bold">
+          Begin {moment(candidateInfo.date_coverage_from).format('MM/DD/YY')}:
+        </div>
+        <div className="col-12 col-md-6 text-right">
+          ${sanitizeNumber(candidateInfo.begin_cash)}
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 col-md-6 text-left font-weight-bold">
+          End {moment(candidateInfo.date_coverage_to).format('MM/DD/YY')}:
+        </div>
+        <div className="col-12 col-md-6 text-right">
+          ${sanitizeNumber(candidateInfo.end_cash)}
+        </div>
+      </div>
+      <div className="w-100 border-bottom my-2" />
+      <div className="row">
+        <div className="col-12 col-md-6 text-left font-weight-bold">
+          Total
+          <span className="text-success font-weight-bold">
+            {' '}
+            Contributions:
+          </span>
+        </div>
+        <div className="col-12 col-md-6 text-right">
+          ${sanitizeNumber(candidateInfo.total_contributions)}
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 col-md-6 text-left font-weight-bold">
+          Total
+          <span className="text-danger font-weight-bold">
+            {' '}
+            Disbursements:
+          </span>
+        </div>
+        <div className="col-12 col-md-6 text-right">
+          ${sanitizeNumber(candidateInfo.total_disbursements)}
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 col-md-6 text-left font-weight-bold">
+          Total
+          <span className="text-success font-weight-bold"> Available:</span>
+        </div>
+        <div className="col-12 col-md-6 text-right">
+          ${sanitizeNumber(
+            candidateInfo.total_contributions -
+            candidateInfo.total_disbursements
+          )}
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 col-md-6 text-left font-weight-bold">
+          Independent Expenditures:
+        </div>
+        <div className="col-12 col-md-6 text-right">
+          ${sanitizeNumber(candidateInfo.independent_expenditures)}
+        </div>
+      </div>
+      <div className="w-100 border-bottom my-2" />
 
       <h2>Usage</h2>
       <div id={candidateInfo.id + '-usage'} />
-      <div className="w-100 border-bottom my-2"></div>
+      <div className="w-100 border-bottom my-2" />
       <h2>Origin</h2>
       <div id={candidateInfo.id + '-total-from'} />
-    </div>
-  );
+    </div>;
 };
 
 export default Finance;
+
+const sanitizeNumber = x => {
+  var parts = x
+    .toFixed(2)
+    .toString()
+    .split('.');
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  parts[1] = parts[1].padEnd(2, 0);
+  return parts.join('.');
+};

e218fdcc81e7e9eb4b9d2ce6b1bdcf1364297e7d
Liv <w.phrush@gmail.com>
2018-08-02 18:53:29 -0400

    rename finance prop to allow committee info as well

--- client/src/components/Finance.jsx
+++ client/src/components/Finance.jsx
@@ -2,28 +2,28 @@ import React from 'react';
 import moment from 'moment';
 import bb from 'billboard.js';
 
-const Finance = ({ data }) => {
-  console.log(data);
+const Finance = ({ candidateInfo, committeeInfo }) => {
+  console.log(candidateInfo, committeeInfo);
   setTimeout(function() {
     var bar = bb.generate({
-      bindto: `#${data.id}-breakdown`,
+      bindto: `#${candidateInfo.id}-breakdown`,
       data: {
         type: 'bar',
         columns: [
-          ['Total Contributions', data.total_contributions],
-          ['Total Disbursements', data.total_disbursements],
-          ['Independent Expenditures', data.independent_expenditures]
+          ['Total Contributions', candidateInfo.total_contributions],
+          ['Total Disbursements', candidateInfo.total_disbursements],
+          ['Independent Expenditures', candidateInfo.independent_expenditures]
         ]
       }
     });
 
     var usage = bb.generate({
-      bindto: `#${data.id}-usage`,
+      bindto: `#${candidateInfo.id}-usage`,
       data: {
         type: 'donut',
         columns: [
-          ['Available', data.total_contributions - data.total_disbursements],
-          ['Disbursed', data.total_disbursements]
+          ['Available', candidateInfo.total_contributions - candidateInfo.total_disbursements],
+          ['Disbursed', candidateInfo.total_disbursements]
         ],
         colors: {
           Available: '#28a745',
@@ -36,12 +36,12 @@ const Finance = ({ data }) => {
     });
 
     var pie = bb.generate({
-      bindto: `#${data.id}-total-from`,
+      bindto: `#${candidateInfo.id}-total-from`,
       data: {
         type: 'donut',
         columns: [
-          ['Individuals', data.total_from_individuals],
-          ['PACs', data.total_from_pacs]
+          ['Individuals', candidateInfo.total_from_individuals],
+          ['PACs', candidateInfo.total_from_pacs]
         ],
         colors: {
           Individuals: '#1b4062',
@@ -58,57 +58,57 @@ const Finance = ({ data }) => {
 
   return (
     <div className="container">
-      <a href="">{data.name}</a>
+      <a href="">{candidateInfo.name}</a>
       <div className="w-100 border-bottom my-2"></div>
       <div className="row">
-        <div className="col-6 text-left font-weight-bold">
-          Begin {moment(data.date_coverage_from).format('MM/DD/YY')}:
+        <div className="col-12 col-md-6 text-left font-weight-bold">
+          Begin {moment(candidateInfo.date_coverage_from).format('MM/DD/YY')}:
         </div>
-        <div className="col-6 text-right">${data.begin_cash}</div>
+        <div className="col-12 col-md-6 text-right">${candidateInfo.begin_cash}</div>
       </div>
       <div className="row">
-        <div className="col-6 text-left font-weight-bold">
-          End {moment(data.date_coverage_to).format('MM/DD/YY')}:
+        <div className="col-12 col-md-6 text-left font-weight-bold">
+          End {moment(candidateInfo.date_coverage_to).format('MM/DD/YY')}:
         </div>
-        <div className="col-6 text-right">${data.end_cash}</div>
+        <div className="col-12 col-md-6 text-right">${candidateInfo.end_cash}</div>
       </div>
       <div className="w-100 border-bottom my-2"></div>
       <div className="row">
-        <div className="col-6 font-weight-bold text-left">
+        <div className="col-12 col-md-6 text-left font-weight-bold">
           Total
           <span className="text-success font-weight-bold"> Contributions:</span>
         </div>
-        <div className="col-6 text-right">${data.total_contributions}</div>
+        <div className="col-12 col-md-6 text-right">${candidateInfo.total_contributions}</div>
       </div>
       <div className="row">
-        <div className="col-6 font-weight-bold text-left">
+        <div className="col-12 col-md-6 text-left font-weight-bold">
           Total
           <span className="text-danger font-weight-bold"> Disbursements:</span>
         </div>
-        <div className="col-6 text-right">${data.total_disbursements}</div>
+        <div className="col-12 col-md-6 text-right">${candidateInfo.total_disbursements}</div>
       </div>
       <div className="row">
-        <div className="col-6 font-weight-bold text-left">
+        <div className="col-12 col-md-6 text-left font-weight-bold">
           Total
           <span className="text-success font-weight-bold"> Available:</span>
         </div>
-        <div className="col-6 text-right">
-          ${(data.total_contributions - data.total_disbursements).toFixed(2)}
+        <div className="col-12 col-md-6 text-right">
+          ${(candidateInfo.total_contributions - candidateInfo.total_disbursements).toFixed(2)}
         </div>
       </div>
       <div className="row">
-        <div className="col-6 font-weight-bold text-left">
+        <div className="col-12 col-md-6 text-left font-weight-bold">
           Independent Expenditures:
         </div>
-        <div className="col-6 text-right">${data.independent_expenditures}</div>
+        <div className="col-12 col-md-6 text-right">${candidateInfo.independent_expenditures}</div>
       </div>
       <div className="w-100 border-bottom my-2"></div>
 
       <h2>Usage</h2>
-      <div id={data.id + '-usage'} />
+      <div id={candidateInfo.id + '-usage'} />
       <div className="w-100 border-bottom my-2"></div>
       <h2>Origin</h2>
-      <div id={data.id + '-total-from'} />
+      <div id={candidateInfo.id + '-total-from'} />
     </div>
   );
 };

db8c6c3ba632ee76fc143642e0ab168a587bbbf6
Liv <w.phrush@gmail.com>
2018-08-02 20:04:27 -0400

    add FinanceRow subcomponent

---
+++ client/src/components/subComponents/FinanceRow.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const FinanceRow = ({
+  prefix,
+  title,
+  amount,
+  color = 'default',
+}) => {
+  return <div className="row">
+      <div className="col-12 col-md-6 text-left font-weight-bold">
+        {prefix + ' '}
+        <span className={`text-${color}`}>{title}</span>:
+      </div>
+      <div className="col-12 col-md-6 text-right">
+        $<span className={`text-${color}`}>{sanitizeNumber(amount)}</span>:
+      </div>
+    </div>;
+};
+
+export default FinanceRow;
+
+const sanitizeNumber = x => {
+  var parts = x
+    .toFixed(2)
+    .toString()
+    .split('.');
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  parts[1] = parts[1].padEnd(2, 0);
+  return parts.join('.');
+};

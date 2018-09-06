a35b2a99c6946578f9af0a820c4eb533b881368a
Liv <w.phrush@gmail.com>
2018-09-06 13:09:17 -0400

    add youtube search terms

--- client/src/config.js
+++ client/src/config.js
@@ -1,10 +1,12 @@
 export default {
   demName: 'Abigail Spanberger',
+  demYouTube: 'Abigail Spanberger',
   demTwitter: 'SpanbergerVA07',
   demMemberId: null,
   demFECId: 'H8VA07094',
   demCommitteeFECId: 'C00649913',
   repName: 'Dave Brat',
+  repYouTube: 'Dave Brat',
   repTwitter: 'RepDaveBrat',
   repMemberId: 'B001290',
   repFECId: 'H4VA07143',

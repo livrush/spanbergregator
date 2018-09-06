dea237d5ab82906f6d4edf3f518602163ea320cb
Liv <w.phrush@gmail.com>
2018-09-06 15:00:22 -0400

    add click function

--- client/src/components/Video.jsx
+++ client/src/components/Video.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import moment from 'moment';
 
-const Video = ({ video }) => (
-  <div className="">
-  <div className="d-flex">
-    <div className="video-thumbnail-wrapper">
-      <img
-        className="video-thumbnail"
-        src={video.snippet.thumbnails.medium.url}
-        alt={video.snippet.title}
-      />
+const Video = ({ video, click }) => (
+  <div onClick={() => click(video.id.videoId)}>
+    <div className="d-flex video-list-item">
+      <div className="video-thumbnail-wrapper">
+        <img
+          className="video-thumbnail"
+          src={video.snippet.thumbnails.medium.url}
+          alt={video.snippet.title}
+        />
+      </div>
+      <p className="video-details">
+        <a
+          href={`https://www.youtube.com/watch?v=${video.id.videoId}`}
+          target="_blank"
+        >
+          {video.snippet.title}
+        </a>
+        <br />
+        {video.snippet.channelTitle}
+        <br />
+        {moment(video.snippet.publishedAt).format('MMM Do, YYYY')}
+      </p>
     </div>
-    <p className="video-details">
-      <a
-        href={`https://www.youtube.com/watch?v=${video.id.videoId}`}
-        target="_blank"
-      >
-        {video.snippet.title}
-      </a>
-      <br />
-      {video.snippet.channelTitle}
-      <br />
-      {moment(video.snippet.publishedAt).format('MMM Do, YYYY')}
-    </p>
-  </div>
   </div>
 );
 
